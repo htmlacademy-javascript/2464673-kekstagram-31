5bf44febda37663667c23420ae9d49d572c49a83
Julia <elnikova_julia@mail.ru>
2024-03-27 17:12:44 +0700

    небольшая правка

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,15 @@
 // функция для проверки строки
-const checkLength = (string, lengthMax) => string.length <= lengthMax;
+// const checkLength = (string, lengthMax) => string.length <= lengthMax;
 
-// функция для проверки на палидром
-const isPalindrome = (string) => {
+// // функция для проверки на палидром
+// const isPalindrome = (string) => {
 
-  string = string.replaceAll(' ', '').toLowerCase();
+//   string = string.replaceAll(' ', '').toLowerCase();
 
-  let reversed = '';
+//   let reversed = '';
 
-  for(let i = string.length - 1; i >= 0; i--) {
-    reversed += string[i];
-  }
-  return string === reversed;
-};
+//   for(let i = string.length - 1; i >= 0; i--) {
+//     reversed += string[i];
+//   }
+//   return string === reversed;
+// };
